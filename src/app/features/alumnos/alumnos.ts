244913f71ec360d7d64ae3a12697b90c6db7e72b
Roberto Agustín Mejía Collazos <robertomejiacollazos@gmail.com>
2025-08-05 01:16:24 -0500

    feat: alumnos & cursos

---
+++ src/app/features/alumnos/alumnos.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-alumnos',
+  imports: [],
+  templateUrl: './alumnos.html',
+  styleUrl: './alumnos.css'
+})
+export class Alumnos {
+
+}
