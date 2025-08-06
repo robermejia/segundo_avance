534a70fd9ffdc887e0da6b950024791dca975010
Roberto Agustín Mejía Collazos <robertomejiacollazos@gmail.com>
2025-08-06 18:52:40 -0500

    feat: enums

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,24 +1,28 @@
 import { Routes } from '@angular/router';
 import { Alumnos } from './features/alumnos/alumnos';
+import { Navegacion } from '../shared/enums/navegacion';
+
 
 export const routes: Routes = [
     {
-        path: "", component: Alumnos  //localhost:3000 
+        path: "", component: Alumnos   //localhost:3000 
     },
     {
-        path: 'alumnos', //preferir no hardcodear rutas
+        path: Navegacion.Alumnos, //Usando Enums
 
         component: Alumnos,
     },
 
     {
-        path: "cursos",
+        path: Navegacion.Cursos, //Usando Enums
+
         /* component: Cursos */
         //lazy loading: cargar el componente Cursos solo cuando se navegue a la ruta /cursos
         loadComponent: () => import('./features/cursos/cursos').then(m => m.Cursos)
     },
     {
-        path: "inscripciones",
+        path: Navegacion.Inscripciones,//Usando Enums
+        
         loadComponent: () => import('./features/inscripciones/inscripciones').then(m => m.Inscripciones)
     },
 
