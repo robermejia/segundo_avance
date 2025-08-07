b0c96e8749d7ec081ec1ccf4de8a4677b64ec88d
Roberto Agustín Mejía Collazos <robertomejiacollazos@gmail.com>
2025-08-06 20:38:54 -0500

    test: getAlumno

--- src/app/features/alumnos/alumnos.ts
+++ src/app/features/alumnos/alumnos.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { AlumnosAPI } from './alumnos-api';
+import { Student } from '../../../shared/entities';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-alumnos',
-  imports: [],
+  imports: [JsonPipe],
   templateUrl: './alumnos.html',
   styleUrl: './alumnos.css'
 })
 export class Alumnos {
+  alumnos: Student[] = [];
+
+  // Inyectar el servicio AlumnosAPI
+  constructor(private alumnosApi: AlumnosAPI) { }
+
+  ngOnInit(): void {
+    this.alumnosApi.getAlumnos().subscribe(alumnos => {
+      this.alumnos = alumnos;
+    });
+  }
 
 }
